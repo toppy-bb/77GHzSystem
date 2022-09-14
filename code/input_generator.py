c4c34876a670a142f26421e99180565973a566dd
toppy-bb <ttoosshhiikkii9912@gmail.com>
2022-09-14 17:29:24 +0900

    reservoir

---
+++ code/input_generator.py
@@ -0,0 +1,15 @@
+import numpy as np
+import signalz
+
+class InputGenerator:
+
+    def __init__(self, start_time, end_time, num_time_steps):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.num_time_steps = num_time_steps 
+
+    def generate_sin(self, amplitude=1.0):
+        return np.sin( np.linspace(self.start_time, self.end_time, self.num_time_steps) ) * amplitude
+
+    def generate_mackey_glass(self, a=0.2, b=1, c=0.9, d=17, e=10, initial=0.1):
+        return (signalz.mackey_glass(self.num_time_steps+200, a=a, b=b, c=c, d=d, e=e, initial=initial) - 0.8)[200:]
---
+++ code/main.py
@@ -0,0 +1,51 @@
+import numpy as np
+from input_generator import InputGenerator
+from reservoir_network import ReservoirNetWork
+import matplotlib.pyplot as plt
+
+T = np.pi * 16 
+RATIO_TRAIN = 0.6
+dt = np.pi * 0.01
+AMPLITUDE = 0.9
+LEAK_RATE=0.02
+NUM_INPUT_NODES = 1
+NUM_RESERVOIR_NODES = 150
+NUM_OUTPUT_NODES = 1
+NUM_TIME_STEPS = int(T/dt)
+
+# example of activator
+def ReLU(x):
+    return np.maximum(0, x)
+
+def main():
+    i_gen = InputGenerator(0, T, NUM_TIME_STEPS)
+    data = i_gen.generate_sin(amplitude=AMPLITUDE)
+    num_train = int(len(data) * RATIO_TRAIN)
+    train_data = data[:num_train]
+
+    model = ReservoirNetWork(inputs=train_data,
+        num_input_nodes=NUM_INPUT_NODES, 
+        num_reservoir_nodes=NUM_RESERVOIR_NODES, 
+        num_output_nodes=NUM_OUTPUT_NODES, 
+        leak_rate=LEAK_RATE)
+
+    model.train() # 訓練
+    train_result = model.get_train_result() # 訓練の結果を取得
+
+    num_predict = int(len(data[num_train:]))
+    predict_result = model.predict(num_predict)
+
+    t = np.linspace(0, T, NUM_TIME_STEPS)
+    ## plot
+    plt.plot(t, data, label="inputs")
+    plt.plot(t[:num_train], train_result, label="trained")
+    plt.plot(t[num_train:], predict_result, label="predicted")
+    plt.axvline(x=int(T * RATIO_TRAIN), label="end of train", color="green") # border of train and prediction
+    plt.legend()
+    plt.title("Echo State Network Sin Prediction")
+    plt.xlabel("time[ms]")
+    plt.ylabel("y(t)")
+    plt.show()
+
+if __name__=="__main__":
+    main()
